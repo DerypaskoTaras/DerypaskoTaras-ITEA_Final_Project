290976f41479df0755917629ab36bf711c313906
DerypaskoTaras <70661921+DerypaskoTaras@users.noreply.github.com>
2020-11-06 12:55:00 +0200

    Fix imports in routers.py

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_restful import Api
-import app.api.resources as res
+from app.api.resources import (
+    UserResources,
+    ProductResources,
+    CategoryResources,
+    CartResources,
+    NewsResources,
+    SuppliersResources
+)
+
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(res.UserResources, '/users', '/users/<user_id>')
-api.add_resource(res.ProductResources, '/products', '/products/<product_id>')
-api.add_resource(res.CategoryResources, '/categories', '/categories/<category_id>')
-api.add_resource(res.CartResources, '/carts', '/carts/<carts_id>')
-api.add_resource(res.NewsResources, '/news', '/news/<news_id>')
-api.add_resource(res.SuppliersResources, '/suppliers', '/suppliers/<supplier_id>')
+api.add_resource(UserResources, '/users', '/users/<user_id>')
+api.add_resource(ProductResources, '/products', '/products/<product_id>')
+api.add_resource(CategoryResources, '/categories', '/categories/<category_id>')
+api.add_resource(CartResources, '/carts', '/carts/<carts_id>')
+api.add_resource(NewsResources, '/news', '/news/<news_id>')
+api.add_resource(SuppliersResources, '/suppliers', '/suppliers/<supplier_id>')
 
 app.run(host='127.0.0.1', port=27018, debug=True)
