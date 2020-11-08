0623782e9bad7c2cc3d35dccd2d6ded6a404ecdc
DerypaskoTaras <70661921+DerypaskoTaras@users.noreply.github.com>
2020-11-08 15:17:58 +0200

    Fix import in routers.py

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from app.api.resources import (
+from .app.api.resources import (
     UserResources,
     ProductResources,
     CategoryResources,
