d24054d11f156c13a9610a730e1594b5571e13f6
DerypaskoTaras <70661921+DerypaskoTaras@users.noreply.github.com>
2020-11-10 15:49:34 +0200

    Fix routers.py

--- app/api/routers.py
+++ app/api/routers.py
@@ -19,5 +19,4 @@ api.add_resource(CartResources, '/carts', '/carts/<carts_id>')
 api.add_resource(NewsResources, '/news', '/news/<news_id>')
 api.add_resource(SuppliersResources, '/suppliers', '/suppliers/<supplier_id>')
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=27018, debug=True)
+app.run(host='127.0.0.1', port=27018, debug=True)
